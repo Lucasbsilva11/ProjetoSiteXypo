b580a24f16f930c10efa036aaaae880af08aa862
Lucas B. Silva <Lucas.bsilva23@hotmail.com>
2022-10-31 20:25:10 -0300

    WebSiteXypo

---
+++ script.js
@@ -0,0 +1,17 @@
+var menuBtn = document.getElementById("menuBtn")
+var sideNav = document.getElementById("sideNav")
+
+sideNav.style.right = "-250px"
+menuBtn.onclick = function(){
+    if(sideNav.style.right == "-250px"){
+        sideNav.style.right = "0"
+    }else{
+        sideNav.style.right = "-250px"
+    }
+}
+
+
+var scroll = new SmoothScroll('a[href*="#"]',{
+    speed: 800,
+    seedAsDuration:true
+});
